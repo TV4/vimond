5440aef1246135062d40587090ba3797fb6f04c1
Peter Hellberg <peter@c7.se>
2018-05-16 11:21:23 +0200

    Add missing comments

--- restapi/videofiles.go
+++ restapi/videofiles.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 )
 
+// Videofiles returns a list of videofiles for the given assetID
 func (c *Client) Videofiles(ctx context.Context, assetID string) (*VideofilesResponse, error) {
 	resp, err := c.get(ctx, c.videofilesPath(assetID), url.Values{})
 	if err != nil {
@@ -42,12 +43,14 @@ func (c *Client) videofilesPath(assetID string) string {
 	return fmt.Sprintf("/api/admin/asset/%s/videofiles", assetID)
 }
 
+// VideofilesResponse is the response returned by the Videofiles method
 type VideofilesResponse struct {
 	AssetID    int         `json:"assetId"`
 	Title      string      `json:"title"`
 	Videofiles []Videofile `json:"videofiles"`
 }
 
+// Videofile has information like bitrate, format, etc. for a video file
 type Videofile struct {
 	Bitrate     int    `json:"bitrate"`
 	MediaFormat string `json:"mediaFormat"`
